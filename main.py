203efb0b00b26524c51d93ec1bf73e48fc2f284b
Jan Dyndor <jandyndor@gmail.com>
2026-09-17 08:28:34 +0200

    RAG pipeline

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # from langchain_anthropic import ChatAnthropic
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 
 # from langchain_openai import ChatOpenAI
